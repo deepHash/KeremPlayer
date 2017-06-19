df11bec86e6c2541a25969331d031983d49442fc
deepHash <itsesisx@gmail.com>
2017-06-19 21:35:39 +0300

    added angular service

---
+++ app/player/src/app/main/player.service.ts
@@ -0,0 +1,26 @@
+import { EventEmitter, Injectable } from '@angular/core';
+import { Http, Response, Headers} from '@angular/http';
+import { Artist } from '../shared/artist.model';
+
+@Injectable()
+export class PlayerService {
+    itemAdded = new EventEmitter<Artist[]>();
+
+    private artist: Artist[] = [];
+
+    constructor(private http: Http) {this.getArtists();}
+
+    private getArtistsDataFromDB() {
+        this.http.get('http://localhost:3000/getAllArtists/new')
+            .subscribe(
+                (response: Response) => {
+                    this.artist = response.json();
+                    this.itemAdded.next(this.artist.slice());
+                    console.log(this.artist);
+                })
+    }
+    getArtists() {
+        this.getArtistsDataFromDB();
+        return this.artist.slice();
+    }
+}
