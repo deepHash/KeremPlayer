91e064c8a6168521b0d3e599022cbc31673ba1df
deepHash <itsesisx@gmail.com>
2017-06-21 03:41:54 +0300

    added model for angular

---
+++ app/player/src/app/shared/mix.model.ts
@@ -0,0 +1,8 @@
+export class Mix {
+
+  constructor(public id: number, 
+              public name: string,
+              public photo: string,
+              public song: [string]) {
+  }
+}
---
+++ app/player/src/app/shared/song.model.ts
@@ -0,0 +1,7 @@
+export class Song {
+
+  constructor(public id: number, 
+              public name: string,
+              public likes: number,) {
+  }
+}
